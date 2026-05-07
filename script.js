635b435f02e6b03bda410f760a9e6c1a3a29c5d9
s0m-thing <gkstha0712@gmail.com>
2026-05-08 02:00:26 +0900

    리뷰 섹션 완성

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.clear;
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 24,
+
+    navigation: {
+        nextEl: "#review-next",
+        prevEl: "#review-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1.2,
+        },
+
+        768: {
+            slidesPerView: 2,
+        },
+
+        1200: {
+            slidesPerView: 4,
+        }
+    }
+});
